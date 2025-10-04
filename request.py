e0bd35f508c4fc3d255a89aad5c903d24f9efdbf
L1ght5t0rm <185170877+L1ght5t0rm@users.noreply.github.com>
2025-10-05 01:32:12 +0800

    Add Files

---
+++ request.py
@@ -0,0 +1,58 @@
+import requests
+
+url=''
+#post_file_path='path/to/your/local/file'
+
+get={
+    'key':'value',
+}
+
+cookies={
+    'key':'value',
+}
+
+headers={
+    'key':'value',
+}
+
+post={
+    'key':'value',
+}
+
+
+
+try:
+    _=post_file_path
+    with open(post_file_path,'rb') as f:
+        files={'file':(post_file_path,f)}
+except NameError:
+    post_file_path=None
+except Exception as e:
+    print(e)
+
+
+
+def tget():
+    x=requests.get(url,params=get,cookies=cookies,headers=headers)
+    print('\n\nResponse:\n',x.text)
+    print('\n\nResponse Cookies:\n\t',x.cookies.get_dict())
+    print('\n\nResponse Headers:')
+    for header,value in x.headers.items(): print(f'\t{header}: {value}')
+    print(x.headers)
+
+def tpost():
+    if post_file_path: x=requests.post(url,params=get,data=post,cookies=cookies,headers=headers,files=files)
+    else: x=requests.post(url,params=get,data=post,cookies=cookies,headers=headers)
+    print('\n\nResponse:\n',x.text)
+    print('\n\nResponse Cookies:\n\t',x.cookies.get_dict())
+    print('Response Headers:')
+    for header,value in x.headers.items(): print(f'{header}: {value}')
+    print(x.headers)
+
+try: tget()
+except Exception as e: print(e)
+print('='*40)
+try: tpost()
+except Exception as e: print(e)
+
+
